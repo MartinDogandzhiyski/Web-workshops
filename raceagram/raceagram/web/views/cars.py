e613072c013542ebe82057f9d80b0388f2d81b5f
Martin Dogandzhiyski <94994594+MartinDogandzhiyski@users.noreply.github.com>
2022-09-13 19:46:15 +0300

    Update cars.py

--- raceagram/raceagram/web/views/cars.py
+++ raceagram/raceagram/web/views/cars.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
-
-from raceagram.forms import CreateCarForm
+from raceagram.forms import CreateCarForm, EditCarForm, DeleteCarForm
 from raceagram.web.helpers import get_profile
 from raceagram.web.models import Car
 
 
 def create_car(request):
+    instance = Car(user_profile=get_profile())
     if request.method == 'POST':
-        form = CreateCarForm(request.POST)
+        form = CreateCarForm(request.POST, instance=instance)
         if form.is_valid():
             form.save()
             return redirect('profile')
     else:
-        form = CreateCarForm()
+        form = CreateCarForm(instance=instance)
 
     context = {
         'form': form
@@ -20,9 +20,35 @@ def create_car(request):
     return render(request, 'car_create.html', context)
 
 
-def edit_car(request):
-    return render(request, 'car_edit.html')
+def edit_car(request, pk):
+    instance = Car.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = EditCarForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = EditCarForm(instance=instance)
+
+    context = {
+        'form': form,
+        'car': instance
+    }
+    return render(request, 'car_edit.html', context)
 
 
-def delete_car(request):
-    return render(request, 'car_delete.html')
+def delete_car(request, pk):
+    instance = Car.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = DeleteCarForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = DeleteCarForm(instance=instance)
+
+    context = {
+        'form': form,
+        'car': instance
+    }
+    return render(request, 'car_delete.html', context)
